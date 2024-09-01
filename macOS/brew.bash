bf7bf9ac3111fff3d0d953d6cb44391d13e1cd20
Muhun Kim <iam@muhun.kim>
2024-09-01 11:32:14 +0900

    Uncomment

--- macOS/brew.bash
+++ macOS/brew.bash
@@ -5,4 +5,4 @@ eval "$(/opt/homebrew/bin/brew shellenv)"
 brew install node neovim
 brew install --cask iterm2 visual-studio-code firefox@developer-edition google-chrome minecraft steam pdf-expert muse telegram-desktop discord raycast gpg-suite
 
-# ln -sf ~/.dotfiles/macOS/karabiner.json ~/.config/karabiner/karabiner.json
+ln -sf ~/.dotfiles/macOS/karabiner.json ~/.config/karabiner
