3c6094174a2ce015b890164365e52428a07c455a
Muhun Kim <iam@muhun.kim>
2024-09-01 11:48:27 +0900

    Use Brewfile

--- macOS/brew.bash
+++ macOS/brew.bash
@@ -2,7 +2,7 @@
 
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
-brew install node neovim
-brew install --cask iterm2 visual-studio-code firefox@developer-edition google-chrome minecraft steam pdf-expert muse telegram-desktop discord raycast gpg-suite
+ln -sf ~/.dotfiles/macOS/Brewfile ~
+brew bundle --file ~/Brewfile
 
 ln -sf ~/.dotfiles/macOS/karabiner.json ~/.config/karabiner
